36170c4e1c0f69e39462f387ae4894bd4d2e027e
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-03-13 21:21:26 +0800

    compile

--- tests/test2.cpp
+++ tests/test2.cpp
@@ -1,7 +1,7 @@
 //
 // Created by ASUS on 2021/3/13.
 //
-/*
+
 #include <cassert>
 #include <DataStructures/WeightedGraph.h>
 #include <iostream>
@@ -42,4 +42,3 @@ int main() {
     assert(wg.GetNeighbors(1).size() == 2);
     assert(wg.GetNeighbors(2).empty());
 }
-*/
