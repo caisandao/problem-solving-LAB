c8186b181cb41c2f07f9d1c663ce14a5a5713b4d
chris_pupupu <191240002@smail.nju.edu.cn>
2021-04-22 18:16:35 +0800

    before week7

--- include/Algorithms/BreadthFirstSearcher.h
+++ include/Algorithms/BreadthFirstSearcher.h
@@ -32,7 +32,7 @@ class BreadthFirstSearcher {
             }
         }
   };
-  static std::optional<int> FindFirstVertex(const TGraph *graph, int start, const std::function<bool(int)> &predicate) {
+  static std::optional<int> TryFindFirstVertex(const TGraph *graph, int start, const std::function<bool(int)> &predicate) {
       if (!graph->ContainsVertex(start))
           return {};
 
