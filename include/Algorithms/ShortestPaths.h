6567053d3c33842544311f6810f779cd9b20011b
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-05-19 20:18:37 +0800

    compile

--- include/Algorithms/ShortestPaths.h
+++ include/Algorithms/ShortestPaths.h
@@ -1,4 +1,4 @@
-/*
+
 #ifndef SHORTEST_PATHS
 #define SHORTEST_PATHS
 
@@ -28,8 +28,9 @@ public:
 };
 
 #endif
-*/
 
+
+/*
 #ifndef SHORTEST_PATHS
 #define SHORTEST_PATHS
 
@@ -82,3 +83,4 @@ public:
 
 
 #endif
+*/
