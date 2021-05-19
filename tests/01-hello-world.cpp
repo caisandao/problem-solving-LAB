75dc2edade1a883c42ce98418d7994265e815117
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-05-19 19:41:18 +0800

    compile

--- tests/01-hello-world.cpp
+++ tests/01-hello-world.cpp
@@ -11,9 +11,10 @@
 using namespace std;
 
 int main() {
+    /*
   Graph g;
 
-  /*
+
    * bool AddVertex(int vertex); *
     bool RemoveVertex(int vertex); *
     bool AddEdge(int vertex1, int vertex2, int weight); *
@@ -54,7 +55,7 @@ int main() {
   assert(g.GetOutgoingEdges(7).size() == 7);
   */
 
-
+  /*
   assert(g.AddVertex(1) == true);
   assert(g.AddVertex(2) == true);
   assert(g.AddVertex(3) == true);
@@ -317,9 +318,45 @@ int main() {
     tmp2.emplace_back(2);
     tmp2.emplace_back(5);
     assert(bs.TryGetShortestPathTo(5) == tmp2);
+    */
 
-    WeightedGraph<int> wgs;
-
+    auto *g = new UndirectedWeightedGraph<int>();
+    for (int i = 1; i <= 6; ++i) {
+        g->AddVertex(i);
+    }
+    int flag = 2;
+    g->AddEdge(1, 2, 1 - flag * 1);
+    g->AddEdge(2, 3, 9 - flag * 2);
+    g->AddEdge(1, 3, 3 - flag * 2);
+    g->AddEdge(3, 4, 1 - flag * 1);
+    g->AddEdge(1, 4, 4 - flag * 2);
+    g->AddEdge(4, 1, 4 - flag * 2);
+    g->AddEdge(5, 6, 5 - flag * 2);
+    g->AddEdge(6, 5, 6 - flag * 2);
+    g->AddEdge(6, 1, 2 - flag * 2);
+    ShortestPaths<UndirectedWeightedGraph<int>> *p = nullptr;
+    for (int i = 1; i <= 6; ++i) {
+        if(flag) {
+            p = new BellmanFordShortestPaths<UndirectedWeightedGraph<int>>(g, i);
+        } else {
+            p = new DijkstraShortestPaths<UndirectedWeightedGraph<int>>(g, i);
+        }
+        for (int j = 1; j <= 6; ++j) {
+            if (j > 1) printf(",");
+            const auto o_path = p->TryGetShortestPathTo(j);
+            if (o_path.has_value()) {
+                int distance = 0;
+                const auto &path = o_path.value();
+                for (int k = 1; k < path.size(); ++k) {
+                    distance += g->GetWeight(path[k - 1], path[k]);
+                }
+                printf("%d", distance);
+            } else {
+                printf("-1");
+            }
+        }
+        printf("\n");
+        delete p;
 }
 
 /*
