cc55a764491c166d36b2116560b64fd5baacea33
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-05-21 11:47:37 +0800

    compile

--- include/Algorithms/FloydShortestPaths.h
+++ include/Algorithms/FloydShortestPaths.h
@@ -6,159 +6,63 @@
 #define GRAPHLIBRARY_FLOYDSHORTESTPATHS_H
 
 #include <Algorithms/MultiSourceShortestPaths.h>
-#include <DataStructures/WeightedEdge.h>
-#include <vector>
-#include <unordered_map>
+#define graph MultiSourceShortestPaths<TGraph>::graph
+#define vertices MultiSourceShortestPaths<TGraph>::vertices
+#define dis MultiSourceShortestPaths<TGraph>::dis
+#define paths MultiSourceShortestPaths<TGraph>::paths
 
 template <typename TGraph>
 class FloydShortestPaths : public MultiSourceShortestPaths<TGraph> {
 private:
-    std::vector<std::vector<bool>> vis;
-    std::vector<std::vector<typename TGraph::value_type>> dis;
-    std::vector<std::vector<int>> paths;
-    std::unordered_map<int, int> vertices; // key is the true vertex, value is the number
-    std::unordered_map<int, int> vertices_inverse; // key is the number, value is the true vertex
-    //std::vector<std::vector<bool>> o_edges;
+    std::vector<int> add_paths(std::vector<int> p1, std::vector<int> p2) {
+        std::vector<int> ret = p1;
+        for (auto & i : p2) {
+            ret.emplace_back(i);
+        }
+        return ret;
+    };
 public:
-    FloydShortestPaths(const TGraph *graph) : MultiSourceShortestPaths<TGraph>(graph) {
-        // initialization of vis and dis
-        std::vector<int> v = graph->GetVertices();
-        for (int i = 0; i < v.size(); i++) {
-            vertices.insert(std::make_pair(v[i], i));
-            vertices_inverse.insert(std::make_pair(i, v[i]));
-        }
-        std::vector<typename TGraph::value_type> tmp;
-        std::vector<bool> tmp_vis;
-        for (int i = 0; i < v.size(); i++) {
-            tmp.emplace_back(typename TGraph::value_type());
-            tmp_vis.emplace_back(false);
-        }
-        // **************************************************
-
-        for (int i = 0; i < v.size(); i++) {
-            dis.emplace_back(tmp);
-            vis.emplace_back(tmp_vis);
-            //o_edges.emplace_back(tmp_vis);
-        }
-
-        // **************************************************
-        for (int i = 0; i < v.size(); i++) {
-            std::vector<WeightedEdge<typename TGraph::value_type>> out_edge = graph->GetOutgoingEdges(v[i]);
-            for (auto & we : out_edge) {
-                dis[i][vertices[we.GetDestination()]] = we.GetWeight();
-                vis[i][vertices[we.GetDestination()]] = true;
-                //o_edges[i][vertices[we.GetDestination()]] = true;
-            }
-        }
-
-        // initialization of paths
-        std::vector<int> tmp_path;
-        for (int i = 0; i < v.size(); i++) {
-            tmp_path.emplace_back(i);
-        }
-        for (int i = 0; i < v.size(); i++) {
-            paths.emplace_back(tmp_path);
-        }
-
-        // initialization of a vertex to itself
-        for (int i = 0; i < v.size(); i++) {
-            vis[i][i] = true;
-        }
-
-        // relax
-        for (int i = 0; i < v.size(); i++) {
-            for (int k = 0; k < v.size(); k++) {
-                for (int j = 0; j < v.size(); j++) {
-                    if ((vis[i][k] && vis[k][j] && !vis[i][j]) ||
-                        (vis[i][k] && vis[k][j] && vis[i][j] && dis[i][j] > dis[i][k] + dis[k][j])) {
-                        dis[i][j] = dis[i][k];
-                        std::optional<std::vector<int>> pkj = TryGetShortestPathTo(vertices_inverse[k], vertices_inverse[j]);
-                        int pt = 1;
-                        while (pt < pkj->size()) {
-                            dis[i][j] = dis[i][j] + graph->GetWeight(pkj->at(pt-1), pkj->at(pt));
-                            paths[i][vertices[pkj->at(pt)]] = vertices[pkj->at(pt-1)];
-                            pt++;
-                        }
-                        vis[i][j] = true;
+    explicit FloydShortestPaths(const TGraph *graph) : MultiSourceShortestPaths<TGraph>(graph) {
+        for (auto & i : vertices) {
+            for (auto & k : vertices) {
+                for (auto & j : vertices) {
+                    if (dis.count({i, k}) &&
+                        dis.count({k, j}) &&
+                        (!dis.count({i, j}) || dis[{i, j}] > dis[{i, k}] + dis[{k, j}])) {
+                        dis[{i, j}] = dis[{i, k}] + dis[{k, j}];
+                        paths[{i, j}] = add_paths(paths[{i, k}], paths[{k, j}]);
                     }
-                    /*
-                    if (vis[i][k] && vis[k][j] && !vis[i][j]) {
-                        if (o_edges[i][k] && paths[i][k] == k) {
-                            dis[i][j] = dis[k][j] + dis[i][k];
-                        } else if (o_edges[k][j] && paths[k][j] == j) {
-                            dis[i][j] = dis[i][k] + dis[k][j];
-                        } else {
-                            dis[i][j] = dis[i][k] + dis[k][j]; // 这里需要左结合和右结合进行区别，看看这来这两个里面有无edge，可以找个东西存一下
-                        }
-                        vis[i][j] = true;
-                    } else if (vis[i][k] && vis[k][j] && vis[i][j] && dis[i][j] > dis[i][k] + dis[k][j]) {
-                        if (o_edges[i][k] && paths[i][k] == k) {
-                            dis[i][j] = dis[k][j] + dis[i][k];
-                        } else if (o_edges[k][j] && paths[k][j] == j) {
-                            dis[i][j] = dis[i][k] + dis[k][j];
-                        } else {
-                            dis[i][j] = dis[i][k] + dis[k][j]; // 这里需要左结合和右结合进行区别，看看这来这两个里面有无edge，可以找个东西存一下
-                        }
-                        vis[i][j] = true;
-                    } else {
-                        continue;
-                    }
-                    */
                 }
             }
         }
-
+        for (auto & i : vertices) {
+            for (auto & j : vertices) {
+                if (paths.count({i, j}))
+                    paths[{i, j}].emplace_back(j);
+            }
+        }
     };
 
-    ~FloydShortestPaths() {
-        vis.clear();
-        dis.clear();
-        paths.clear();
-        vertices.clear();
-        vertices_inverse.clear();
-    };
+    ~FloydShortestPaths() = default;
+
     bool HasPathTo(int source, int destination) const {
-        if (!this->graph->ContainsVertex(source) || !this->graph->ContainsVertex(destination))
-            return false;
-        if (vis[vertices.at(source)][vertices.at(destination)])
-            return true;
-        else
-            return false;
+        if (!graph->ContainsVertex(source) || !graph->ContainsVertex(destination)) return false;
+        if (dis.count({source, destination})) return true;
+        else return false;
     };
+
     std::optional<typename TGraph::value_type> TryGetDistanceTo(int source, int destination) const {
-        if (!HasPathTo(source, destination))
+        if (HasPathTo(source, destination))
+            return dis[{source, destination}];
+        else
             return std::nullopt;
-        else {
-            return dis[vertices.at(source)][vertices.at(destination)];
-        }
     };
+
     std::optional<std::vector<int>> TryGetShortestPathTo(int source, int destination) const {
-        if (!HasPathTo(source, destination))
+        if (HasPathTo(source, destination))
+            return paths[{source, destination}];
+        else
             return std::nullopt;
-        else {
-            std::vector<int> tmp;
-            if (source == destination) {
-                tmp.emplace_back(source);
-                tmp.emplace_back(destination);
-                return tmp;
-            } else {
-                int sou_num = vertices.at(source);
-                int des_num = vertices.at(destination);
-                //int pt = paths[sou_num][des_num];
-                //tmp.emplace_back(destination);
-                int pt = des_num;
-                do {
-                    tmp.emplace_back(vertices_inverse.at(pt));
-                    pt = paths[sou_num][pt];
-                } while (pt != paths[sou_num][pt]);
-                tmp.emplace_back(source);
-                std::vector<int> tmp2;
-                for (int i = tmp.size() - 1; i >= 0; i--) {
-                    tmp2.emplace_back(tmp[i]);
-                }
-                return tmp2;
-            }
-        }
     };
 };
 
--- include/Algorithms/MultiSourceShortestPaths.h
+++ include/Algorithms/MultiSourceShortestPaths.h
@@ -2,18 +2,39 @@
 #define MULTI_SOURCE_SHORTEST_PATHS
 
 #include <vector>
+#include <map>
 #include <optional>
 
 template <typename TGraph>
 class MultiSourceShortestPaths {
 protected:
     const TGraph *graph;
+    typedef typename TGraph::value_type TValue;
+    std::map<std::pair<int, int>, TValue> dis;
+    std::map<std::pair<int, int>, std::vector<int>> paths;
+    std::vector<int> vertices;
 public:
   MultiSourceShortestPaths() = delete;
   explicit MultiSourceShortestPaths(const TGraph *graph) {
       this->graph = graph;
+      vertices = graph->GetVertices();
+      for (auto & vertex : vertices) {
+          dis.emplace(std::make_pair(vertex, vertex), TValue());
+          paths.emplace(std::make_pair(vertex, vertex), std::vector<int>{vertex, vertex});
+      }
+      for (auto & vertex : vertices) {
+          std::vector<int> tmp = graph->GetNeighbors(vertex);
+          for (auto & des : tmp) {
+              dis.emplace(std::make_pair(vertex, des), graph->GetWeight(vertex, des));
+              paths.emplace(std::make_pair(vertex, des), std::vector<int>{vertex});
+          }
+      }
   };
-  virtual ~MultiSourceShortestPaths() = default;
+  virtual ~MultiSourceShortestPaths() {
+      dis.clear();
+      paths.clear();
+      vertices.clear();
+  };
 public:
   virtual bool HasPathTo(int source, int destination) const = 0;
   virtual std::optional<typename TGraph::value_type> TryGetDistanceTo(int source, int destination) const = 0;
