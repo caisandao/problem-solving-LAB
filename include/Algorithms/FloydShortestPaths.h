c738612dbed367d7fd95dfbae75591607320af47
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-05-21 13:59:47 +0800

    compile

--- include/Algorithms/FloydShortestPaths.h
+++ include/Algorithms/FloydShortestPaths.h
@@ -1,7 +1,7 @@
 //
 // Created by ASUS on 2021/5/19.
 //
-#ifndef CHECK
+
 #ifndef GRAPHLIBRARY_FLOYDSHORTESTPATHS_H
 #define GRAPHLIBRARY_FLOYDSHORTESTPATHS_H
 
@@ -23,7 +23,7 @@ private:
     };
 public:
     explicit FloydShortestPaths(const TGraph *graph) : MultiSourceShortestPaths<TGraph>(graph) {
-        for (auto & k : vertices) {
+        for (auto & k : vertices) { // rnm,记住floyd三重循环的顺序
             for (auto & i : vertices) {
                 for (auto & j : vertices) {
                     if (dis.count({i, k}) &&
@@ -68,65 +68,4 @@ public:
 };
 
 #endif //GRAPHLIBRARY_FLOYDSHORTESTPATHS_H
-#endif
 
-#ifdef CHECK
-#ifndef GRAPHLIBRARY_FLOYDSHORTESTPATHS_H
-#define GRAPHLIBRARY_FLOYDSHORTESTPATHS_H
-
-#include "Algorithms/MultiSourceShortestPaths.h"
-//#include "Exceptions/NegativeCycleException.h"
-#define vertices MultiSourceShortestPaths<TGraph>::vertices
-#define Distance MultiSourceShortestPaths<TGraph>::Distance
-#define Path     MultiSourceShortestPaths<TGraph>::Path
-
-template<typename TGraph>
-class FloydShortestPaths : public MultiSourceShortestPaths<TGraph>{
-public:
-    typedef typename TGraph::value_type TValue;
-public:
-    explicit FloydShortestPaths(const TGraph *graph) : MultiSourceShortestPaths<TGraph>(graph){
-        std::map<std::pair<int, int>, int> mid;
-//        return ;
-
-        for (int k : vertices)
-            for (int i : vertices) {
-//                if (i == k) continue;
-                for (int j : vertices) {
-//                    if (j == i || j == k)   continue;
-                    if (!Distance.count({i, k})|| !Distance.count({k, j}))
-                        continue;
-                    TValue new_dis = Distance.at({i, k}) + Distance.at({k, j});
-                    if (Distance.count({i, j}) == 1 && Distance.at({i, j}) <= new_dis)
-                        continue;
-                    Distance[{i, j}] = new_dis;
-                    if (mid.count({k, j}) == 1)
-                        mid[{i, j}]= mid[{k, j}];
-                    else
-                        mid[{i, j}] = k;
-                }
-
-            }
-
-//        printf("suc\n");
-
-        for (int src : vertices)
-            for (int dst : vertices){
-                if (src == dst) continue;
-                if (!Distance.count({src, dst}))  continue;
-                Path[{src, dst}].insert(Path[{src, dst}].begin(), dst);
-                int tmp = dst;
-                while(mid.count({src, tmp}) == 1) {
-                    tmp = mid[{src, tmp}];
-                    Path[{src, dst}].insert(Path[{src, dst}].begin(), tmp);
-                }
-                Path[{src, dst}].insert(Path[{src, dst}].begin(), src);
-            }
-
-    }
-
-    ~FloydShortestPaths() = default;
-};
-
-#endif //GRAPHLIBRARY_FLOYDSHORTESTPATHS_H
-#endif
--- include/Algorithms/MultiSourceShortestPaths.h
+++ include/Algorithms/MultiSourceShortestPaths.h
@@ -1,4 +1,3 @@
-#ifndef CHECK
 #ifndef MULTI_SOURCE_SHORTEST_PATHS
 #define MULTI_SOURCE_SHORTEST_PATHS
 
@@ -41,67 +40,4 @@ public:
   virtual std::optional<typename TGraph::value_type> TryGetDistanceTo(int source, int destination) const = 0;
   virtual std::optional<std::vector<int>> TryGetShortestPathTo(int source, int destination) const = 0;
 };
-#endif
-#endif
-
-#ifdef CHECK
-#ifndef MULTI_SOURCE_SHORTEST_PATHS
-#define MULTI_SOURCE_SHORTEST_PATHS
-
-#include <vector>
-#include <optional>
-#include <DataStructures/WeightedEdge.h>
-
-template<typename TGraph>
-class MultiSourceShortestPaths {
-public:
-    typedef typename TGraph::value_type TValue;
-
-protected:
-    std::map<std::pair<int, int>, TValue> Distance;
-    std::map<std::pair<int, int>, std::vector<int>> Path;
-    std::vector<int> vertices;
-public:
-    MultiSourceShortestPaths() = delete;
-
-    explicit MultiSourceShortestPaths(const TGraph *graph) {
-        vertices = graph->GetVertices();
-        for (int src : vertices) {
-            Distance.emplace(std::make_pair(src, src), TValue());
-            Path.emplace(std::make_pair(src, src), std::vector<int>{src});
-            for (const int dst : graph->GetNeighbors(src))
-                Distance.emplace(std::make_pair(src, dst), graph->GetWeight(src, dst));
-        }
-    }
-
-    virtual ~MultiSourceShortestPaths() {
-        Distance.clear();
-        Path.clear();
-        vertices.clear();
-    }
-
-public:
-    [[nodiscard]] bool HasPathTo(int source, int destination) const {
-        if (std::find(vertices.begin(), vertices.end(), source) == vertices.end())
-            return false;
-        if (std::find(vertices.begin(), vertices.end(), destination) == vertices.end())
-            return false;
-        return Distance.count(std::make_pair(source, destination)) == 1;
-    }
-
-    std::optional<TValue> TryGetDistanceTo(int source, int destination) const {
-        if (HasPathTo(source, destination))
-            return Distance.at(std::make_pair(source, destination));
-        return std::nullopt;
-    }
-
-    [[nodiscard]] std::optional<std::vector<int>> TryGetShortestPathTo(int source, int destination) const {
-        if (!HasPathTo(source, destination))
-            return std::nullopt;
-        return Path.at(std::make_pair(source, destination));
-    }
-};
-
-
-#endif
 #endif
